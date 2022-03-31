5967f5987388e004475f514038ce26fe76ea3888
swapnilgadekar9211 <swapnilgadekar9211@gmail.com>
2022-03-31 13:26:36 +0530

    conditions task

--- task/task2.js
+++ task/task2.js
@@ -1,4 +1,30 @@
-//1.Will alert be shown?
+/*1NO:
+1.Will alert be shown?-->it will show alert
 if ("0") {
   alert( 'Hello' );
+}*/
+//3.Creat grade calculator app
+let per=90
+let grade;
+
+if (per>=90){
+
+  console.log("grade will be A")
+
 }
+else if (per>=80){
+
+  console.log("grade will be B")
+
+}
+else if (per>=70){
+
+  console.log("grade will be C")
+
+}
+
+else{
+  console.log("student is failed")
+}
+
+let sub=
