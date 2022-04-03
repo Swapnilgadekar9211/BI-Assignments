bf4ee3c5625b4806f773d445b9aa509ad22eb1d1
swapnilgadekar9211 <swapnilgadekar9211@gmail.com>
2022-04-03 15:59:22 +0530

    Explanation added

--- task/task2.js
+++ task/task2.js
@@ -1,9 +1,24 @@
-/*1NO:
-1.Will alert be shown?-->it will show alert
+/*Problem no1
+1.Will alert be shown?
+//-->Yes alert will shown because,
+when we give string values to if condition i.e string "0" it will consider 
+it as a true value & it will show the o/p.
 if ("0") {
   alert( 'Hello' );
 }*/
 
+
+//Problem no2
+let NUMBER=Number(prompt("enter number"))
+if(NUMBER>0){
+    alert("+1")
+}
+else if(NUMBER<0){
+    alert("-1")
+}
+else if(NUMBER=0){
+    alert("0")
+}
 
 //3.Creat grade calculator app
 let per=20
